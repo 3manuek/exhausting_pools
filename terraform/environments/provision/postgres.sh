e5ee320b8c82e60f50168c64012ecef16788cd04
emanuel_ongres <3manuek@gmail.com>
2019-12-04 18:47:05 -0300

    Added disk to database node

--- terraform/environments/provision/postgres.sh
+++ terraform/environments/provision/postgres.sh
@@ -7,13 +7,17 @@ deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main
 EOF
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-apt-get update
+apt-get update -y
 
-apt install postgresql-client-9.6
-apt install postgresql-9.6
+apt install -y postgresql-client-9.6 postgresql-9.6 parted
 
 # Initialize disk
 
+mkdir /data 2> /dev/null
+parted dev/sdb mklabel data
+parted -a optimal /dev/sdb mkpart primary 0% 100%
+mkfs.xfs /dev/sdb 
+mount -o defaults,nofail,noatime,noexec,nodiratime,noquota /dev/sdb1 /data
 
 # init data on it
 
