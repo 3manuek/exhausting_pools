abea0adbad59eeb3b8f30a57b6b284ef4fd1f0ca
emanuel_ongres <emanuel@ongres.com>
2019-12-09 17:13:43 -0300

    small fix to lib

--- terraform/modules/compute/files/lib.sh
+++ terraform/modules/compute/files/lib.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 function getDbIp {
-    gcloud --format=json compute instances list --filter="tags:(database)" | jq -r '.[].networkInterfa
-ces[].networkIP'
+    gcloud --format=json compute instances list --filter="tags:(database)" | jq -r '.[].networkInterfaces[].networkIP'
 }
