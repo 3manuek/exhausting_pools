67790e86b1d3d104e97f91d225c9c752a3b79bef
Kadaffy Talavera <kadaffy@ongres.com>
2019-12-17 18:41:28 +0100

    Update ansible inventory script
    Signed-off-by: Kadaffy Talavera <kadaffy@ongres.com>

--- ansible_hosts_inventory.sh
+++ ansible_hosts_inventory.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-abspath=$(pwd)
+project_dir=$(pwd)
 
 set -x
 
-cd ${abspath}/terraform/environments/testing
+cd ${project_dir}/terraform/environments/testing
 export odyssey_external_ip=`terraform output -json | jq -r .odyssey_external_ip.value[0][0]`
 export postgres_external_ip=`terraform output -json | jq -r .postgres_external_ip.value[0][0]`
 
@@ -13,12 +13,12 @@ then
   exit 1
 fi
 
-if [ -s ${abspath}/ansible/inventory/hosts.yaml ]
+if [ -s ${project_dir}/ansible/inventory/hosts.yaml ]
 then
-  rm ${abspath}/ansible/inventory/hosts.yaml
+  rm ${project_dir}/ansible/inventory/hosts.yaml
 fi
 
-cat >> ${abspath}/ansible/inventory/hosts.yaml << EOF
+cat >> ${project_dir}/ansible/inventory/hosts.yaml << EOF
 
 all:
   children:
