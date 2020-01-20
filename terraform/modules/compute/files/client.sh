24113d599a3f63414dc9b2ab4b2be97e72a5bdab
3manuek <emanuel@ongres.com>
2020-01-20 18:13:40 +0100

    Continued adding client setup

--- terraform/modules/compute/files/client.sh
+++ terraform/modules/compute/files/client.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 source /usr/local/bin/lib.sh
+
+useradd postgres 
+
+apt update
+
+apt install -y jq htop linux-tools-`uname -r`
+
+
+cat > /etc/apt/sources.list.d/pgdg.list <<EOF
+deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main
+EOF
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+apt-get update
+
+# pg version reason is to stick to GL's version,only headers do not match
+apt install -y postgresql-contrib-9.6 postgresql-client-9.6
+
+mkdir /var/lib/postgresql
+echo "*:*:*:user_bench:Odybench*" >> /var/lib/postgresql/.pgpass
+chmod 0600 /var/lib/postgresql/.pgpass
+
